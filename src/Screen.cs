8c52032b15e82397e47acb682cb6f204ad25c614
Charles Pascoe <cpascoe95@gmail.com>
2015-10-23 19:36:37 +0100

    Added Screen class

---
+++ src/Screen.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Snake {
+    public class Screen {
+        public bool CursorVisible {
+            get { return Console.CursorVisible; }
+            set { Console.CursorVisible = value; }
+        }
+
+        public int Width {
+            get { return Console.WindowWidth; }
+        }
+
+        public int Height {
+            get { return Console.WindowHeight; }
+        }
+
+        public ConsoleColor Background {
+            get { return Console.BackgroundColor; }
+            set { Console.BackgroundColor = value; }
+        }
+
+        public ConsoleColor Foreground {
+            get { return Console.ForegroundColor; }
+            set { Console.ForegroundColor = value; }
+        }
+
+        public Screen() {
+            this.CursorVisible = false;
+            this.Clear();
+        }
+
+        public void Clear() {
+            Console.Clear();
+        }
+
+        public void SetBlock(int x, int y, char c) {
+            if ((int)c > 0 && x >= 0 && y >= 0 && x < this.Width && y < this.Width) {
+                Console.SetCursorPosition(x, y);
+                Console.Write(c);
+            }
+        }
+    }
+}
+
