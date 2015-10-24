8e629f696388ca3111b7d6f1d8aaed88ed80dd65
Charles Pascoe <cpascoe95@gmail.com>
2015-10-24 10:49:26 +0100

    Added more test code to Program

--- src/Program.cs
+++ src/Program.cs
@@ -1,21 +1,39 @@
 using System;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace Snake {
     public class Program {
         public static void Main(string[] args) {
             SnakeBody s = new SnakeBody();
+            Box b = new Box(30, 30, 10, 3);
+            b.background = ConsoleColor.Blue;
+            // b.borderBackground = ConsoleColor.DarkBlue;
+            b.borderForeground = ConsoleColor.Yellow;
 
             Screen screen = new Screen();
 
-            for (int i = 0; i < 10; i++) {
-                screen.Clear();
+            List<string> ts = new List<string>();
+
+            for (int i = 0; i < 100; i++) {
+                DateTime start = DateTime.Now;
                 s.Draw(screen, new Vector(i, 0));
+                b.Draw(screen, new Vector(0, 0));
+                TimeSpan t1 = DateTime.Now - start;
                 screen.Draw();
-                Thread.Sleep(500);
+                TimeSpan t2 = DateTime.Now - start;
+
+                ts.Add(t1.Milliseconds.ToString() + " " + t2.Milliseconds.ToString());
+
+                int timeout = 50 - t2.Milliseconds;
+
+                if (timeout < 0) timeout = 0;
+                Thread.Sleep(timeout);
             }
 
-            Thread.Sleep(5000);
+            foreach (string t in ts) {
+                Console.WriteLine(t);
+            }
         }
     }
 }
