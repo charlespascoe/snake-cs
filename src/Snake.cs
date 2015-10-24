1c75fd538c2a0f88bcc4794b196bfde4c63edb52
Charles Pascoe <cpascoe95@gmail.com>
2015-10-24 18:11:55 +0100

    Added move logic

--- src/Snake.cs
+++ src/Snake.cs
@@ -1,12 +1,18 @@
 using System;
 using System.Collections.Generic;
 using Snake.Graphics;
+using Snake.UI;
 
 namespace Snake {
     public class SnakeBody : IDrawable {
         private Head head = new Head();
         private Tail tail = new Tail();
         private List<BodySegment> body = new List<BodySegment>();
+
+        public Direction MoveDirection { get; set; }
+
+        private int ticksBetweenMoves = 10;
+        private int ticksUntilNextMove = 10;
 
         public SnakeBody() {
             this.head.Position.X = 10;
@@ -32,7 +38,42 @@ namespace Snake {
         }
 
         public void Update() {
+            ticksUntilNextMove--;
 
+            if (UserInput.KeyPressed) {
+                switch (UserInput.Key) {
+                    case ConsoleKey.W:
+                        this.MoveDirection = Direction.Up;
+                        break;
+                    case ConsoleKey.A:
+                        this.MoveDirection = Direction.Left;
+                        break;
+                    case ConsoleKey.S:
+                        this.MoveDirection = Direction.Down;
+                        break;
+                    case ConsoleKey.D:
+                        this.MoveDirection = Direction.Right;
+                        break;
+                }
+            }
+
+            if (ticksUntilNextMove <= 0) {
+                this.Move();
+                ticksUntilNextMove = ticksBetweenMoves;
+            }
+        }
+
+        public void Move() {
+            Vector oldHeadPos = this.head.Position;
+
+            this.head.Position += this.MoveDirection.ToVector();
+
+            BodySegment b = this.body.Pop();
+
+            this.tail.Position = b.Position;
+
+            b.Position = oldHeadPos;
+            this.body.Insert(0, b);
         }
 
         public void Draw(Screen screen, Vector parentPos) {
