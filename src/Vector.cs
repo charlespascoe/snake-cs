a75031b5c0c7ed85e568cc759e1fdb42a6dccfcd
Charles Pascoe <cpascoe95@gmail.com>
2015-10-24 08:21:21 +0100

    Added Vector class

---
+++ src/Vector.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Snake {
+    public class Vector {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Vector(int x, int y) {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public static Vector operator +(Vector v1, Vector v2) {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+        public static Vector operator -(Vector v1, Vector v2) {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y);
+        }
+
+        public static Vector operator %(Vector v1, Vector v2) {
+            return new Vector(v1.X % v2.X, v1.Y % v2.Y);
+        }
+    }
+}
+
