53bfbc1d4398b24c4187d1b948ecf0a46a03749a
Charles Pascoe <cpascoe95@gmail.com>
2015-10-24 09:37:11 +0100

    Changed Screen to use a buffer (of Cells) to determine what to draw to the screen

--- src/Screen.cs
+++ src/Screen.cs
@@ -7,41 +7,76 @@ namespace Snake {
             set { Console.CursorVisible = value; }
         }
 
-        public int Width {
-            get { return Console.WindowWidth; }
+        public int Width { get; private set; }
+
+        public int Height { get; private set; }
+
+        public ConsoleColor DefaultForeground { get; set; }
+        public ConsoleColor DefaultBackground { get; set; }
+
+        public Screen() : this(Console.WindowWidth, Console.WindowHeight) {
         }
 
-        public int Height {
-            get { return Console.WindowHeight; }
-        }
+        private Cell[,] buffer;
 
-        public ConsoleColor Background {
-            get { return Console.BackgroundColor; }
-            set { Console.BackgroundColor = value; }
-        }
+        public Screen(int width, int height) {
+            if (width < 0 || height < 0) {
+                throw new ArgumentOutOfRangeException("width and height must be greater than 0");
+            }
 
-        public ConsoleColor Foreground {
-            get { return Console.ForegroundColor; }
-            set { Console.ForegroundColor = value; }
-        }
+            this.buffer = new Cell[width, height];
+            this.Width = width;
+            this.Height = height;
 
-        public Screen() {
-            this.CursorVisible = false;
             this.Clear();
+
+            this.CursorVisible = false;
+
+            this.DefaultForeground = ConsoleColor.White;
+            this.DefaultBackground = ConsoleColor.Black;
         }
 
         public void Clear() {
+            for (int x = 0; x < this.Width; x++) {
+                for (int y = 0; y < this.Height; y++) {
+                    this.buffer[x, y] = new Cell();
+                }
+            }
             Console.Clear();
         }
 
         public void SetCell(Vector v, char c) {
-            this.SetCell(v.X, v.Y, c);
+            this.SetCell(v, c, this.DefaultForeground, this.DefaultBackground);
+        }
+
+        public void SetCell(Vector v, char c, ConsoleColor f, ConsoleColor b) {
+            this.SetCell(v.X, v.Y, c, f, b);
         }
 
         public void SetCell(int x, int y, char c) {
-            if ((int)c > 0 && x >= 0 && y >= 0 && x < this.Width && y < this.Width) {
-                Console.SetCursorPosition(x, y);
-                Console.Write(c);
+            this.SetCell(x, y, c, this.DefaultForeground, this.DefaultBackground);
+        }
+
+        public void SetCell(int x, int y, char c, ConsoleColor f, ConsoleColor b) {
+            if ((int)c > 0 && x >= 0 && y >= 0 && x < this.Width && y < this.Height) {
+                Cell cell = this.buffer[x, y];
+                cell.character = c;
+                cell.foreground = f;
+                cell.background = b;
+            }
+        }
+
+        public void Draw() {
+            for (int x = 0; x < this.Width; x++) {
+                for (int y = 0; y < this.Height; y++) {
+                    Cell cell = this.buffer[x, y];
+                    if (cell.HasChanged) {
+                        Console.ForegroundColor = cell.foreground;
+                        Console.BackgroundColor = cell.background;
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(cell.character);
+                    }
+                }
             }
         }
     }
