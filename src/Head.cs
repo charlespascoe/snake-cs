77e032f03049b480b3ff550ef6c65b32fec1d98f
Charles Pascoe <cpascoe95@gmail.com>
2015-10-24 19:12:29 +0100

    Added Head.HeadDirection

--- src/Head.cs
+++ src/Head.cs
@@ -2,6 +2,29 @@ using System;
 
 namespace Snake {
     public class Head : BodySegment {
+        private Direction _HeadDirection = Direction.Up;
+        public Direction HeadDirection {
+            get { return this._HeadDirection; }
+            set {
+                this._HeadDirection = value;
+
+                switch (value) {
+                    case Direction.Up:
+                        this.BodyChar = 'v';
+                        break;
+                    case Direction.Left:
+                        this.BodyChar = '>';
+                        break;
+                    case Direction.Down:
+                        this.BodyChar = '^';
+                        break;
+                    case Direction.Right:
+                        this.BodyChar = '<';
+                        break;
+                }
+            }
+        }
+
         public Head() {
             this.BodyChar = '<';
             this.Foreground = ConsoleColor.White;
