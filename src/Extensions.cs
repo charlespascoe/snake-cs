799dea24e0a0a196f0e89bb70a35bef551aaa9f2
Charles Pascoe <cpascoe95@gmail.com>
2015-10-27 15:17:21 +0000

    Added MaxLength, Center, LeftPad and RightPad String extensions

--- src/Extensions.cs
+++ src/Extensions.cs
@@ -28,6 +28,34 @@ namespace Snake {
         public static T Pop<T>(this List<T> list) {
             return list.Pop(list.Count - 1);
         }
+
+        public static String MaxLength(this string text, int maxLength) {
+            return text.Length > maxLength ? text.Substring(0, maxLength) : text;
+        }
+
+        public static String Center(this string text, int length) {
+            if (text.Length >= length) return text;
+
+            int padding = length - text.Length;
+
+            string result = new String(' ', padding / 2) + text + new String(' ', padding / 2);
+
+            if (result.Length < length) {
+                result += ' ';
+            }
+
+            return result;
+        }
+
+        public static String LeftPad(this string text, char padding, int length) {
+            int addPadding = length - text.Length;
+            return addPadding <= 0 ? text : new String(padding, addPadding) + text;
+        }
+
+        public static String RightPad(this string text, char padding, int length) {
+            int addPadding = length - text.Length;
+            return addPadding <= 0 ? text : text + new String(padding, addPadding);
+        }
     }
 }
 
