fd67d40e1d421eeb4f72e9668a6d2504be13f66d
Charles Pascoe <cpascoe95@gmail.com>
2015-11-07 19:29:28 +0000

    Added Pause Menu

--- src/Game.cs
+++ src/Game.cs
@@ -8,8 +8,7 @@ namespace Snake {
         private List<IPositionable> layouts = new List<IPositionable>();
         private GameArea gameArea;
         private ScoreBox scoreBox;
-
-
+        private PauseMenu pauseMenu;
 
         public Game(Vector screenSize, DifficultySettings settings) : base(screenSize) {
             VerticalLayout verticalLayout = new VerticalLayout(new Vector(8, 4), screenSize - new Vector(16, 8), LayoutSizing.Center);
@@ -25,22 +24,42 @@ namespace Snake {
             verticalLayout.AddChild(scoreBox);
 
             this.layouts.Add(verticalLayout);
+
+            Vector pauseMenuSize = new Vector(16, 12);
+            Vector pauseMenuPos = verticalLayout.Position + this.gameArea.Position + (this.gameArea.Size - pauseMenuSize) / 2;
+            this.pauseMenu = new PauseMenu(pauseMenuPos, pauseMenuSize);
+            this.pauseMenu.OnResume += new EventHandler(this.OnResume);
         }
 
         public override void Update() {
+            if (!this.gameArea.Paused  && !UserInput.InputHandled && UserInput.Key == ConsoleKey.Escape) {
+                this.gameArea.Paused = true;
+                UserInput.InputHandled = true;
+                this.pauseMenu.Focus();
+            }
+
             foreach (IPositionable layout in this.layouts) {
                 layout.Update();
             }
+
+            this.pauseMenu.Update();
         }
 
         public override void Draw(Screen screen) {
             foreach (IPositionable layout in this.layouts) {
                 layout.Draw(screen, new Vector());
             }
+
+            this.pauseMenu.Draw(screen, new Vector());
         }
 
         private void OnScoreChange(object sender, EventArgs e) {
             this.scoreBox.Score = this.gameArea.Score;
+        }
+
+        private void OnResume(object sender, EventArgs e) {
+            this.gameArea.Paused = false;
+            this.pauseMenu.Blur();
         }
     }
 }
