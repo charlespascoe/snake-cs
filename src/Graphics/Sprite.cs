c4ceb8b44f69791197bbc1aa32c2a22f3b2bcb65
Charles Pascoe <cpascoe95@gmail.com>
2016-02-11 18:45:13 +0000

    Sprite now extends from CellMatrix

--- src/Graphics/Sprite.cs
+++ src/Graphics/Sprite.cs
@@ -1,9 +1,7 @@
 using System;
 
 namespace Snake.Graphics {
-    public class Sprite : IPositionable {
-        private Cell[,] cells;
-
+    public class Sprite : CellMatrix, IPositionable {
         private Vector _Position;
         public Vector Position {
             get { return this._Position; }
@@ -14,25 +12,15 @@ namespace Snake.Graphics {
             }
         }
 
-        private Vector _Size;
-        public Vector Size {
-            get { return this._Size; }
+        public new Vector Size {
+            get { return base.Size; }
             set {
                 // Silently do nothing
             }
         }
 
-        public Sprite(Vector position, Vector size) {
-            this._Size = size;
+        public Sprite(Vector position, Vector size) : base(size) {
             this.Position = position;
-
-            this.cells = new Cell[size.X, size.Y];
-
-            for (int x = 0; x < size.X; x++) {
-                for (int y = 0; y < size.Y; y++) {
-                    this.cells[x, y] = new Cell((char)0, ConsoleColor.White, ConsoleColor.Black);
-                }
-            }
         }
 
         public void Update() {
@@ -40,9 +28,8 @@ namespace Snake.Graphics {
         }
 
         public void Draw(Screen screen, Vector parentPos) {
-
+            screen.SetCells(parentPos + this.Position, this.cells);
         }
-
     }
 }
 
