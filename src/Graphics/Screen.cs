30f4a0dcbca81d24ee93aab6f314a1c9b42b3499
Charles Pascoe <charles@cpascoe.co.uk>
2017-02-10 18:47:34 +0000

    Screen: Refactor to use Colour

--- src/Graphics/Screen.cs
+++ src/Graphics/Screen.cs
@@ -1,17 +1,26 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 
 namespace Snake.Graphics {
     public class Screen : CellMatrix {
+        private const byte ESCAPE_BYTE = 27;
+
+        private Stream stdout;
+
         public bool CursorVisible {
             get { return Console.CursorVisible; }
             set { Console.CursorVisible = value; }
         }
 
+        private Colour currentForeground;
+        private Colour currentBackground;
+
         // Subtract 1, because when the cursor writes the last line,
         // it will go to the next line, pushing everything up by 1
-        public Screen() : base(new Vector(Console.WindowWidth, Console.WindowHeight - 1), ' ', ConsoleColor.White, ConsoleColor.Black) {
+        public Screen() : base(new Vector(Console.WindowWidth, Console.WindowHeight - 1), ' ', Colour.White, Colour.Black) {
             this.CursorVisible = false;
+            this.stdout = Console.OpenStandardOutput();
         }
 
         private int drawChangedThreshold = 1000;
@@ -33,19 +42,19 @@ namespace Snake.Graphics {
 
             List<char> writeBuffer = new List<char>();
 
-            Console.ForegroundColor = this.cells[0, 0].Foreground;
-            Console.BackgroundColor = this.cells[0, 0].Background;
+            this.SetForeground(this.cells[0, 0].Foreground);
+            this.SetBackground(this.cells[0, 0].Background);
 
             for (int y = 0; y < this.Size.Y; y++) {
                 for (int x = 0; x < this.Size.X; x++) {
                     Cell cell = this.cells[x, y];
 
-                    if (cell.Foreground != Console.ForegroundColor || cell.Background != Console.BackgroundColor) {
+                    if (cell.Foreground != this.currentForeground || cell.Background != this.currentBackground) {
                         Console.Write(writeBuffer.ToArray());
                         writeBuffer.Clear();
 
-                        Console.ForegroundColor = cell.Foreground;
-                        Console.BackgroundColor = cell.Background;
+                        this.SetForeground(cell.Foreground);
+                        this.SetBackground(cell.Background);
                     }
 
                     writeBuffer.Add(cell.Character);
@@ -70,14 +79,32 @@ namespace Snake.Graphics {
                 for (int x = 0; x < this.Size.X; x++) {
                     Cell cell = this.cells[x, y];
                     if (cell.HasChanged) {
-                        Console.ForegroundColor = cell.Foreground;
-                        Console.BackgroundColor = cell.Background;
+                        this.SetForeground(cell.Foreground);
+                        this.SetBackground(cell.Background);
                         Console.SetCursorPosition(x, y);
                         Console.Write(cell.Character);
                         cell.AfterDraw();
                     }
                 }
             }
+        }
+
+        private void SetForeground(Colour col) {
+            if (col == this.currentForeground) return;
+            this.currentForeground = col;
+
+            this.stdout.WriteByte(ESCAPE_BYTE);
+
+            this.stdout.Write($"[38;5;{col.ToAnsiColourCode()}m");
+        }
+
+        private void SetBackground(Colour col) {
+            if (col == this.currentBackground) return;
+            this.currentBackground = col;
+
+            this.stdout.WriteByte(ESCAPE_BYTE);
+
+            this.stdout.Write($"[48;5;{col.ToAnsiColourCode()}m");
         }
     }
 }
