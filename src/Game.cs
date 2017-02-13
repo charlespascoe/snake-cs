7d0501a1fc2ff45b10204e831b2d3badbd9e50e9
Charles Pascoe <charles@cpascoe.co.uk>
2017-02-13 15:34:31 +0000

    Game: Implement Game Over message after death

--- src/Game.cs
+++ src/Game.cs
@@ -9,26 +9,39 @@ namespace Snake {
         private GameArea gameArea;
         private ScoreBox scoreBox;
         private PauseMenu pauseMenu;
+        private bool gameOver;
+        private int ticksUntilGameEnded = 100;
+        private GameOverPanel gameOverPanel;
 
         public Game(Vector screenSize, DifficultySettings settings) : base(screenSize) {
             VerticalLayout verticalLayout = new VerticalLayout(new Vector(8, 4), screenSize - new Vector(16, 8));
 
             Vector scoreBoxSize = new Vector(8, 3);
 
-            this.gameArea = new GameArea(new Vector(), verticalLayout.Size - new Vector(0, 5), settings);
+            StackLayout stackLayout = new StackLayout(new Vector(), verticalLayout.Size - new Vector(0, scoreBoxSize.Y + 2));
+
+            // Game Area
+            this.gameArea = new GameArea(new Vector(), stackLayout.Size, settings);
             this.gameArea.OnScoreChange += this.OnScoreChange;
+            this.gameArea.OnDeath += this.OnDeath;
+            stackLayout.AddChild(this.gameArea);
 
+            // Pause Menu
+            this.pauseMenu = new PauseMenu(new Vector(), new Vector(16, 12));
+            this.pauseMenu.OnResume += this.OnResume;
+            stackLayout.AddChild(new Container(this.pauseMenu, horizontalPos: HorizontalPosition.Center, verticalPos: VerticalPosition.Center));
+
+            // Game Over
+            this.gameOverPanel = new GameOverPanel();
+            stackLayout.AddChild(new Container(this.gameOverPanel, horizontalPos: HorizontalPosition.Center, verticalPos: VerticalPosition.Center));
+
+            verticalLayout.AddChild(stackLayout);
+
+            // Score box
             this.scoreBox = new ScoreBox(new Vector(), scoreBoxSize);
-
-            verticalLayout.AddChild(gameArea);
             verticalLayout.AddChild(new Container(scoreBox, horizontalPos: HorizontalPosition.Center, verticalPos: VerticalPosition.Center));
 
             this.layouts.Add(verticalLayout);
-
-            Vector pauseMenuSize = new Vector(16, 12);
-            Vector pauseMenuPos = verticalLayout.Position + this.gameArea.Position + (this.gameArea.Size - pauseMenuSize) / 2;
-            this.pauseMenu = new PauseMenu(pauseMenuPos, pauseMenuSize);
-            this.pauseMenu.OnResume += this.OnResume;
         }
 
         public override void Update() {
@@ -43,18 +56,30 @@ namespace Snake {
             }
 
             this.pauseMenu.Update();
+
+            if (this.gameOver) {
+                this.ticksUntilGameEnded--;
+
+                if (this.ticksUntilGameEnded == 0) {
+                    this.gameArea.FlashBorder = false;
+                    this.gameOverPanel.Score = this.scoreBox.Score;
+                    this.gameOverPanel.Visible = true;
+                }
+            }
         }
 
         public override void Draw(Screen screen) {
             foreach (IPositionable layout in this.layouts) {
                 layout.Draw(screen, new Vector());
             }
-
-            this.pauseMenu.Draw(screen, new Vector());
         }
 
         private void OnScoreChange(object sender, EventArgs e) {
             this.scoreBox.Score = this.gameArea.Score;
+        }
+
+        private void OnDeath(object sender, EventArgs e) {
+            this.gameOver = true;
         }
 
         private void OnResume(object sender, EventArgs e) {
