877db2453a7148809890e0d323838643fc701841
Charles Pascoe <charles@cpascoe.co.uk>
2017-02-13 15:32:02 +0000

    Sprite: Add FromStringArray static method

--- src/Graphics/Sprite.cs
+++ src/Graphics/Sprite.cs
@@ -15,6 +15,34 @@ namespace Snake.Graphics {
             this.Position = position;
         }
 
+        public Sprite(Vector position, Vector size, char defaultChar, Colour foreground, Colour background) : base(size, defaultChar, foreground, background) {
+            this.Position = position;
+        }
+
+        public static Sprite FromStringArray(string[] spriteContent) {
+            return Sprite.FromStringArray(spriteContent, Colour.White, Colour.Black);
+        }
+
+        public static Sprite FromStringArray(string[] spriteContent, Colour foreground, Colour background) {
+            int height = spriteContent.Length;
+
+            if (height == 0) return new Sprite(new Vector(), new Vector());
+
+            int width = spriteContent[0].Length;
+
+            if (width == 0) return new Sprite(new Vector(), new Vector());
+
+            Sprite sprite = new Sprite(new Vector(), new Vector(width, height), ' ', foreground, background);
+
+            for (int i = 0; i < height; i++) {
+                if (spriteContent[i].Length != width) throw new Exception("Sprite content must be rectangular");
+
+                sprite.DrawString(new Vector(0, i), spriteContent[i]);
+            }
+
+            return sprite;
+        }
+
         public void Update() {
 
         }
