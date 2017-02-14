876e207277132c3fe0c9e1c3870cc3e239c4e2a5
Charles Pascoe <charles@cpascoe.co.uk>
2017-02-14 13:03:36 +0000

    Food: Change food character to unicode dot

--- src/Food.cs
+++ src/Food.cs
@@ -15,8 +15,8 @@ namespace Snake {
 
         public override void Draw(Screen screen, Vector parentPos) {
             Vector screenPos = parentPos + new Vector(this.GamePosition.X * 2, this.GamePosition.Y);
-            screen.SetCell(screenPos, '[', Colour.Yellow, Colour.Black);
-            screen.SetCell(screenPos + new Vector(1, 0), ']', Colour.Yellow, Colour.Black);
+            screen.SetCell(screenPos, (char)0x25CF, Colour.Yellow, Colour.Black);
+            screen.SetCell(screenPos + new Vector(1, 0), ' ', Colour.Yellow, Colour.Black);
         }
     }
 }
