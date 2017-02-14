b9502c378e82b1eb2eee76bbb6aa3cd8f1d3ade0
Charles Pascoe <charles@cpascoe.co.uk>
2017-02-14 09:41:49 +0000

    GameOverPanel: Add Restart/Quit options

--- src/GameOverPanel.cs
+++ src/GameOverPanel.cs
@@ -1,3 +1,4 @@
+using System;
 using Snake.Graphics;
 using Snake.UI;
 
@@ -13,17 +14,59 @@ namespace Snake {
 
         private VerticalLayout rootLayout;
 
-        public int Score { get; set; }
+        private Text scoreText;
+
+        private Lense lense = new Lense();
+
+        private int _Score;
+        public int Score {
+            get { return this._Score; }
+            set {
+                this._Score = value;
+                this.scoreText.Content = $"Your Score: {value}";
+            }
+        }
+
+        public event EventHandler OnRestart;
 
         public bool Visible { get; set; }
 
-        private int score;
-        public GameOverPanel() : base(new Vector(), new Vector(64, 12)) {
+        public GameOverPanel() : base(new Vector(), new Vector(64, 15)) {
             this.Style = new RoundedBoxStyle();
             this.Style.BorderForeground = Colour.Red;
 
-            this.rootLayout = new VerticalLayout(new Vector(1, 1), this.Size - new Vector(2, 2));
+            this.scoreText = new Text("Your Score:", new Vector(0, 1), horizontalAlignment: HorizontalTextAlignment.Center);
+
+            this.rootLayout = new VerticalLayout(new Vector(2, 1), this.Size - new Vector(4, 2));
+            this.rootLayout.Spacing = 2;
             this.rootLayout.AddChild(new Container(this.gameOverSprite, horizontalPos: HorizontalPosition.Center));
+            this.rootLayout.AddChild(this.scoreText);
+
+            HorizontalLayout horizontalLayout = new HorizontalLayout(new Vector(), new Vector(0, 3));
+            horizontalLayout.Spacing = 2;
+
+            Button restartBtn = new Button("Restart");
+            restartBtn.Size = new Vector((this.rootLayout.Size.X / 2) - 1, 3);
+            restartBtn.OnClick += (sender, e) => this.OnRestart?.Invoke(this, EventArgs.Empty);
+
+            Button quitBtn = new Button("Quit");
+            quitBtn.Size = new Vector((this.rootLayout.Size.X / 2) - 1, 3);
+            quitBtn.OnClick += (sender, e) => Program.Quit();
+
+            horizontalLayout.AddChild(restartBtn);
+            horizontalLayout.AddChild(quitBtn);
+
+            this.rootLayout.AddChild(horizontalLayout);
+
+            this.lense.AddChild(restartBtn);
+            this.lense.AddChild(quitBtn);
+
+            this.lense.Focus();
+        }
+
+        public override void Update() {
+            base.Update();
+            this.rootLayout.Update();
         }
 
         public override void Draw(Screen screen, Vector parentPos) {
@@ -31,11 +74,7 @@ namespace Snake {
 
             base.Draw(screen, parentPos);
 
-            this.gameOverSprite.Draw(screen, parentPos + this.Position + new Vector(1, 1));
-
-            string yourScore = $"Your Score: {this.Score}";
-
-            screen.DrawString(parentPos + this.Position + new Vector((this.Size.X - yourScore.Length) / 2, this.Size.Y - 3), yourScore);
+            this.rootLayout.Draw(screen, parentPos + this.Position);
         }
     }
 }
